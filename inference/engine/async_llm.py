5ab4993dfe4f6bdd55115f92412a8a148d08c14c
Dawid <dawid.sliwka321@gmail.com>
2025-09-24 12:55:03 +0200

    working version

--- inference/engine/async_llm.py
+++ inference/engine/async_llm.py
@@ -1,7 +1,10 @@
 import asyncio
 from collections import deque
 from dataclasses import dataclass
-from typing import List, Union
+import queue
+import traceback
+from typing import List, Optional, Union
+from uuid import uuid4
 
 import torch
 from inference.config.model_config import ModelConfig
@@ -9,15 +12,25 @@ from inference.engine.kv_cache import KVCache
 from inference.model_loader.model_loader import ModelLoader
 from transformers import AutoTokenizer, PreTrainedTokenizer, PreTrainedTokenizerFast
 
-from inference.models.qwen3 import Qwen3ModelInstance
+from inference.models.qwen3continous import (
+    ForwardBatch,
+    ForwardMode,
+    Qwen3ModelInstance,
+)
 from inference.openai_protocol import ChatCompletionRequest
+import logging
 
 
 @dataclass
-class ActiveRequest:
+class InferenceRequest:
     id: str
     max_new_tokens: int
-    cache: KVCache
+    request: ChatCompletionRequest
+    out_tokens: asyncio.Queue
+    cache: Optional[KVCache] = None
+    last_token: Optional[int] = None
+    cur_len: Optional[int] = None
+    remaining: Optional[int] = None
 
 
 DEVICE = "cpu"
@@ -33,51 +46,198 @@ class AsyncLLM:
         self.tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = (
             AutoTokenizer.from_pretrained(model_config.model_path)
         )
-        self.tokenizer.padding_side = "left"
+        self.eos_id = self.tokenizer.eos_token_id
 
         self.max_slots = 6
         self.max_ctx_length = 1024
-        self.prefill_queue = asyncio.Queue()
+        self.prefill_queue: queue.Queue[InferenceRequest] = queue.Queue()
         self.slots = deque([i for i in range(self.max_slots)])
-        self.active = {}
+        self.active: dict[str, InferenceRequest] = {}
 
-    async def create_chat_completion(self, request: ChatCompletionRequest):
-        request_prompt = self.tokenizer.apply_chat_template(
-            conversation=request.messages,
-            add_generation_prompt=True,
-            enable_thinking=False,
-        )
-        eos_id = self.tokenizer.eos_token_id
+        self._worker: Optional[asyncio.Task] = None
+        self._shutdown = asyncio.Event()
 
-        # 1. PREFILL
-        prompt_ids = torch.tensor(request_prompt).unsqueeze(0)
-        pos_ids = torch.arange(
-            prompt_ids.shape[1],
-        ).unsqueeze(0)
-        with torch.no_grad():
-            logits = self.model(prompt_ids, pos_ids, prefill=True)
+    def start(self):
+        if self._worker is None:
+            self._worker = asyncio.create_task(self._worker_task())
 
-        next_id = torch.argmax(logits[:, -1, :], dim=1, keepdim=True)
-        input_ids = torch.cat([prompt_ids, next_id], dim=1)
-        curr_len = input_ids.shape[1]
+    def stop(self):
+        if self._worker is not None:
+            self._shutdown.set()
+            self._worker.cancel()
 
-        for _ in range(128):
-            last_token = input_ids[:, -1:]
-            pos_ids = torch.tensor([[curr_len - 1]], device=input_ids.device)
-
-            with torch.no_grad():
-                logits = self.model(last_token, pos_ids, prefill=False)
-
-            next_id = torch.argmax(logits[:, -1, :], dim=1, keepdim=True)
-
-            if next_id.item() == eos_id:
+    async def _worker_task(self):
+        while not self._shutdown.is_set():
+            logging.info("running engine loop")
+            try:
+                await self.accept_request()
+                if self.active:
+                    await self.engine_step()
+                    await asyncio.sleep(0)
+                else:
+                    await asyncio.sleep(1)
+            except Exception as e:
+                traceback.print_exc()
+                logging.error(f"hit error: {e}")
                 break
 
-            input_ids = torch.cat([input_ids, next_id], dim=1)
-            curr_len += 1
+    async def accept_request(self):
+        while self.slots and not self.prefill_queue.empty():
+            req = self.prefill_queue.get()
+            logging.info(f"accepting request: {req.id} for prefill")
+            slot = self.slots.popleft()
 
-        return self.tokenizer.decode(input_ids.squeeze(0).tolist())
+            token_ids = self.tokenizer.apply_chat_template(
+                conversation=req.request.messages,
+                add_generation_prompt=True,
+                enable_thinking=False,
+            )
+            lengths = torch.tensor([len(token_ids)], dtype=torch.long)
+            total_len = int(lengths.sum())
+            bsz = lengths.numel()
+            start_offsets = torch.zeros(bsz, dtype=torch.long)
+            flat_tokens = torch.tensor(token_ids, dtype=torch.long)
+            batch_of_pos = torch.repeat_interleave(
+                torch.arange(bsz, dtype=torch.long), lengths
+            )
+            global_idx = torch.arange(total_len, dtype=torch.long)
 
+            pos_in_seq = global_idx - start_offsets[batch_of_pos]
+
+            same_batch = batch_of_pos[:, None] == batch_of_pos[None, :]
+            causal = pos_in_seq[:, None] >= pos_in_seq[None, :]
+
+            allowed = same_batch & causal
+            attn_mask = torch.where(
+                allowed,
+                torch.zeros((), device=allowed.device),
+                torch.full((), float("-inf"), device=allowed.device),
+            )
+
+            end_positions = start_offsets + lengths - 1
+            kv_cache = KVCache.new(
+                num_hidden_layers=self.config.num_hidden_layers,
+                ctx_length=4096,
+                num_kv_heads=self.config.num_key_value_heads,
+                head_dim=self.config.head_dim,
+            )
+            fwd_batch_prefill = ForwardBatch(
+                forward_mode=ForwardMode.PREFILL,
+                lengths=lengths,
+                start_offsets=start_offsets,
+                batch_of_pos=batch_of_pos,
+                pos_in_seq=pos_in_seq,
+                attn_mask=attn_mask,
+                kv_caches=[kv_cache],
+                end_positions=end_positions,
+            )
+
+            with torch.inference_mode():
+                logits = self.model(
+                    input_ids=flat_tokens,
+                    position_ids=pos_in_seq,
+                    forward_batch=fwd_batch_prefill,
+                )
+                next_ids = torch.argmax(logits[end_positions], dim=1, keepdim=True)
+                new_ids = next_ids.flatten(-2, 1)
+                # we try index to 0 because we do on sequence per forward pass in the prefill
+                next_token = self.tokenizer.decode(new_ids[0])
+                await req.out_tokens.put(next_token)
+
+            req.cur_len = len(token_ids)
+            req.remaining = self.max_ctx_length - req.cur_len
+            req.last_token = int(new_ids[0].item())
+            req.cache = kv_cache
+            self.active[slot] = req
+
+    async def prefill(self, request: InferenceRequest):
+        self.prefill_queue.put(request, block=False)
+
+    async def engine_step(self):
+        slots = list(self.active.keys())
+        bsz = len(slots)
+        lengths = []
+        kv_caches = []
+        tokens = []
+        for slot_id in slots:
+            slot_request = self.active[slot_id]
+            lengths.append(slot_request.cur_len)
+            tokens.append(slot_request.last_token)
+            kv_caches.append(slot_request.cache)
+
+        token_ids = torch.tensor(tokens, dtype=torch.long)
+        decode_lengths = torch.tensor(lengths, dtype=torch.long)
+        pos_ids = torch.arange(bsz, dtype=torch.long)
+
+        bsz = decode_lengths.numel()
+        start_offsets_decode = torch.zeros(bsz, dtype=torch.long)
+        if bsz > 1:
+            start_offsets_decode[1:] = torch.cumsum(decode_lengths[:-1], dim=0)
+
+        batch_of_pos_keys = torch.repeat_interleave(
+            torch.arange(bsz, dtype=torch.long), decode_lengths
+        )
+
+        allowed = pos_ids[:, None] == batch_of_pos_keys[None, :]
+
+        attn_mask_decode = torch.where(
+            allowed,
+            torch.zeros((), device=allowed.device),
+            torch.full((), float("-inf"), device=allowed.device),
+        )
+        pos_in_seq_decode = decode_lengths - 1
+        fwd_batch_decode = ForwardBatch(
+            forward_mode=ForwardMode.DECODE,
+            lengths=decode_lengths,
+            start_offsets=start_offsets_decode,
+            batch_of_pos=pos_ids,
+            pos_in_seq=pos_in_seq_decode,
+            attn_mask=attn_mask_decode,
+            kv_caches=kv_caches,
+        )
+        with torch.inference_mode():
+            logits = self.model(
+                input_ids=token_ids,
+                position_ids=pos_in_seq_decode,
+                forward_batch=fwd_batch_decode,
+            )
+            next_ids = torch.argmax(logits, dim=1, keepdim=True)
+            new_ids = next_ids.flatten(-2, 1)
+        finished = []
+        for idx, slot_id in enumerate(slots):
+            slot_request = self.active[slot_id]
+            
+            request_token = int(new_ids[idx].item())
+            if request_token == self.eos_id or slot_request.remaining == 0:
+                await slot_request.out_tokens.put(None)
+                finished.append(slot_id)
+                continue
+
+            detokenized = self.tokenizer.decode(request_token)
+            await slot_request.out_tokens.put(detokenized)
+            slot_request.cur_len += 1
+            slot_request.remaining -= 1
+            slot_request.last_token = request_token
+
+        for finished_id in finished:
+            del self.active[finished_id]
+            self.slots.append(finished_id)
+            
 
     async def chat_cmpl_continous_batching(self, request: ChatCompletionRequest):
-        pass
+        chat_cmpl_request = InferenceRequest(
+            id=str(uuid4()),
+            max_new_tokens=self.max_ctx_length,
+            cache=None,
+            request=request,
+            out_tokens=asyncio.Queue(),
+        )
+        logging.info(f"putting a request with id:{chat_cmpl_request.id} for prefill")
+        await self.prefill(chat_cmpl_request)
+
+        while True:
+            token = await chat_cmpl_request.out_tokens.get()
+            logging.info("generation")
+            if token is None:
+                break
+            yield token
--- inference/models/qwen3continous.py
+++ inference/models/qwen3continous.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import StrEnum, auto
 from typing import List, Optional
 from torch import nn
@@ -5,13 +6,8 @@ import torch
 from inference.config.model_config import ModelConfig
 from transformers import AutoConfig
 
-from dataclasses import dataclass
-from typing import Union
-
-from transformers import AutoTokenizer, PreTrainedTokenizer, PreTrainedTokenizerFast
-
 from inference.engine.kv_cache import KVCache
-from inference.model_loader.model_loader import ModelLoader
+
 
 
 class ForwardMode(StrEnum):
@@ -27,8 +23,9 @@ class ForwardBatch:
     batch_of_pos: torch.Tensor
     pos_in_seq: torch.Tensor
     attn_mask: torch.Tensor
-    end_positions: torch.Tensor
     kv_caches: List[KVCache]
+    end_positions: Optional[torch.Tensor] = None
+
 
 
 def compute_rope_params(
@@ -332,226 +329,3 @@ class Qwen3ModelInstance(nn.Module):
             forward_batch,
         )
 
-
-model_config = ModelConfig(model_path="Qwen/Qwen3-0.6B")
-
-
-tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = (
-    AutoTokenizer.from_pretrained(model_config.model_path)
-)
-
-requests = [
-    {
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are helpful assistant, answer user questions",
-            },
-            {"role": "user", "content": "Hello how are you feeling today"},
-        ]
-    },
-    {
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are helpful assistant, answer user questions",
-            },
-            {"role": "user", "content": "Tell me a funny story about a dog."},
-        ]
-    },
-    {
-        "messages": [
-            {"role": "user", "content": "Tell me a funny story about cat."},
-        ]
-    },
-]
-
-conversations = []
-for request in requests:
-    conversations.append(request["messages"])
-
-
-token_ids = tokenizer.apply_chat_template(
-    conversation=conversations,
-    add_generation_prompt=True,
-    enable_thinking=False,
-)
-
-lengths = torch.tensor([len(r) for r in token_ids], dtype=torch.long)
-total_len = int(lengths.sum())
-bsz = lengths.numel()
-start_offsets = torch.zeros(bsz, dtype=torch.long)
-if bsz > 1:
-    start_offsets[1:] = torch.cumsum(lengths[:-1], dim=0)
-
-flat_tokens = torch.tensor(
-    [token for seq in token_ids for token in seq], dtype=torch.long
-)
-batch_of_pos = torch.repeat_interleave(torch.arange(bsz, dtype=torch.long), lengths)
-global_idx = torch.arange(total_len, dtype=torch.long)
-
-pos_in_seq = global_idx - start_offsets[batch_of_pos]
-
-
-same_batch = batch_of_pos[:, None] == batch_of_pos[None, :]
-causal = pos_in_seq[:, None] >= pos_in_seq[None, :]
-
-allowed = same_batch & causal
-attn_mask = torch.where(
-    allowed,
-    torch.zeros((), device=allowed.device),
-    torch.full((), float("-inf"), device=allowed.device),
-)
-caches = []
-for _ in lengths:
-    caches.append(
-        KVCache.new(
-            num_hidden_layers=model_config.hf_config.num_hidden_layers,
-            ctx_length=4096,
-            num_kv_heads=model_config.hf_config.num_key_value_heads,
-            head_dim=model_config.hf_config.head_dim,
-        )
-    )
-
-end_positions = start_offsets + lengths - 1
-fwd_batch_prefill = ForwardBatch(
-    forward_mode=ForwardMode.PREFILL,
-    lengths=lengths,
-    start_offsets=start_offsets,
-    batch_of_pos=batch_of_pos,
-    pos_in_seq=pos_in_seq,
-    attn_mask=attn_mask,
-    kv_caches=caches,
-    end_positions=end_positions,
-)
-
-
-model_inst = Qwen3ModelInstance(config=model_config)
-loader = ModelLoader()
-loader.load_model(config=model_config, model=model_inst)
-
-
-new_ids = []
-
-with torch.no_grad():
-    logits = model_inst(
-        input_ids=flat_tokens, position_ids=pos_in_seq, forward_batch=fwd_batch_prefill
-    )
-
-    next_ids = torch.argmax(logits[end_positions], dim=1, keepdim=True)
-    print(next_ids)
-    new_ids = next_ids.flatten(-2, 1)
-    print(new_ids.shape)
-
-
-sequences, output_seq_len = next_ids.shape
-
-decode_lengths = []
-
-for idx in range(sequences):
-    decode_lengths.append(len(next_ids[idx]))
-decode_lengths = torch.tensor(decode_lengths)
-bsz = decode_lengths.numel()
-
-start_offsets_decode = torch.zeros(bsz, dtype=torch.long)
-if bsz > 1:
-    start_offsets_decode[1:] = torch.cumsum(decode_lengths[:-1], dim=0)
-
-# for idx in range(len(lengths)):
-#     lengths[idx] += 1
-
-# batch_of_pos_decode = torch.tensor([0, 1, 2])
-# batch_of_pos_keys = torch.repeat_interleave(
-#     torch.arange(bsz, dtype=torch.long), lengths
-# )
-
-# allowed = batch_of_pos_decode[:, None] == batch_of_pos_keys[None, :]
-
-# attn_mask_decode = torch.where(
-#     allowed,
-#     torch.zeros((), device=allowed.device),
-#     torch.full((), float("-inf"), device=allowed.device),
-# )
-# pos_in_seq_decode = lengths - 1
-
-# fwd_batch_decode = ForwardBatch(
-#     forward_mode=ForwardMode.DECODE,
-#     lengths=decode_lengths,
-#     start_offsets=start_offsets_decode,
-#     batch_of_pos=batch_of_pos_decode,
-#     pos_in_seq=pos_in_seq_decode,
-#     kv_caches=caches,
-#     attn_mask=attn_mask_decode,
-#     end_positions=end_positions,
-# )
-
-with torch.no_grad():
-    # logits = model_inst(
-    #     input_ids=new_ids,
-    #     position_ids=pos_in_seq_decode,
-    #     forward_batch=fwd_batch_decode,
-    # )
-
-    # next_ids = torch.argmax(logits, dim=1, keepdim=True)
-    new_ids = next_ids.flatten(-2, 1)
-    res = [[], [], []]
-
-    for _ in range(32):
-        for idx in range(len(lengths)):
-            lengths[idx] += 1
-
-        batch_of_pos_decode = torch.tensor([0, 1, 2])
-        batch_of_pos_keys = torch.repeat_interleave(
-            torch.arange(bsz, dtype=torch.long), lengths
-        )
-
-        allowed = batch_of_pos_decode[:, None] == batch_of_pos_keys[None, :]
-
-        attn_mask_decode = torch.where(
-            allowed,
-            torch.zeros((), device=allowed.device),
-            torch.full((), float("-inf"), device=allowed.device),
-        )
-        pos_in_seq_decode = lengths - 1
-        print(pos_in_seq_decode)
-        fwd_batch_decode = ForwardBatch(
-            forward_mode=ForwardMode.DECODE,
-            lengths=decode_lengths,
-            start_offsets=start_offsets_decode,
-            batch_of_pos=batch_of_pos_decode,
-            pos_in_seq=pos_in_seq_decode,
-            kv_caches=caches,
-            attn_mask=attn_mask_decode,
-            end_positions=end_positions,
-        )
-        logits = model_inst(
-            input_ids=new_ids,
-            position_ids=pos_in_seq_decode,
-            forward_batch=fwd_batch_decode,
-        )
-        next_ids = torch.argmax(logits, dim=1, keepdim=True)
-        new_ids = next_ids.flatten(-2, 1)
-        for idx in range(3):
-            res[idx].append(new_ids[idx].item())
-
-
-for i in range(len(res)):
-    print(tokenizer.decode(res[i]))
-    print("\n")
-# [[ 39814,      0,   5692,    594,    264,  15173,   3364,
-#             911,    264,   8251,   1447,  12522,   5193,    264,    882,     11,
-#            1052,    572,    264,   8251,   6941,   3070,     47,  29877,    334,
-#              13,   3776,   1899,     11,    393,  29877,   6635,    311,    728,
-#             389,    264,   3070,      1,   4616,   4227,      1,    334,    311,
-#           13186,    279,   1879,     13,   1988,    438,    566,  14858,     11,
-#             566,   2684,   5558,    323,   9482,    705,    304,    264,   3070,
-#           50655,    334,     13,   2619,     11,    566,   2270,    264,   3070,
-#              70,   5372,  90524,    334,   6941,   3070,     50,  86640,    334,
-#              13,    576,  90524,    572,   1602,  11657,    323,   1053,     11,
-#             330,  18665,     11,   8251,      0,   3555,    525,    498,   3730,
-#             304,    279,  13638,   7521,    393,  29877,  19995,     11,    330,
-#              40,   2776,   1101,  23966,   8958,    328,  86640,  31527,    323,
-#            1053,     11,    330,  11908,     11,    358,   2776,    537,    264,
-#            8251,      0,    358,   2776,    264]]
-
-# [tensor(0), tensor(358), tensor(2776), tensor(1588), tensor(311), tensor(1492), tensor(498), tensor(448), tensor(4113), tensor(498), tensor(1184), tensor(13), tensor(2585), tensor(525), tensor(498), tensor(8266), tensor(3351), tensor(30), tensor(26525), tensor(232), tensor(151645), tensor(198), tensor(151643), tensor(33975), tensor(25), tensor(358), tensor(2776), tensor(8266), tensor(2167), tensor(1661), tensor(3351), tensor(13)]
